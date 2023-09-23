1befa68ce9199be7eb425ed1c145217813e89de1
Kanxak <142275299+Kanxak@users.noreply.github.com>
2023-09-23 15:53:54 +1200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+// images
+import Logo from  '../assets/Logo.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href='http://localhost:3000/static/media/Logo.23bf098723dbd86767e7d6084d88a61f.svg'>
+          <img src={Logo} alt='Sumtingwrong' />
+        </a>
+        {/* button */}
+        <button className='btn btn-sm'>Work with me</button>
+        </div>
+        </div>
+        </header>
+        );
 };
 
 export default Header;
